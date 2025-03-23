717136db76de33a8e97501e72b64108b513ff9e1
mertunctuncer <mertunctuncer@gmail.com>
2025-03-23 14:26:17 +0300

    Code cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,31 @@
 plugins {
     id("java")
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "com.github.mertunctuncer"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
+description = "A simple player vault plugin."
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    compileJava {
+        options.release = 21
+    }
+
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,12 @@
 rootProject.name = "player-vaults"
 
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
         maven("https://repo.papermc.io/repository/maven-public/")
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
